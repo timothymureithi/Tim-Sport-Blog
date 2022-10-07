b9446ed9ee5b1fa108387f77f9364da64ccb1f76
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-07 23:53:40 +0300

    Updates

---
+++ precache-manifest.d00d486d1306bb1a04dd000da99c2573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b402b124a853143a2927",
+    "url": "/Tim-Sport-Blog/static/css/main.a1f3239d.chunk.css"
+  },
+  {
+    "revision": "b402b124a853143a2927",
+    "url": "/Tim-Sport-Blog/static/js/main.b402b124.chunk.js"
+  },
+  {
+    "revision": "2b447e1be3e7dd9deafc",
+    "url": "/Tim-Sport-Blog/static/js/1.2b447e1b.chunk.js"
+  },
+  {
+    "revision": "1eed0b56b182e7f83703",
+    "url": "/Tim-Sport-Blog/static/css/2.63a5e766.chunk.css"
+  },
+  {
+    "revision": "1eed0b56b182e7f83703",
+    "url": "/Tim-Sport-Blog/static/js/2.1eed0b56.chunk.js"
+  },
+  {
+    "revision": "5d364867b13b2b9b4e12",
+    "url": "/Tim-Sport-Blog/static/js/runtime~main.5d364867.js"
+  },
+  {
+    "revision": "1f8d8d63ebfe9705a5fa4cf5e16c2b67",
+    "url": "/Tim-Sport-Blog/index.html"
+  }
+];
